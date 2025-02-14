b129d0ab9afe905b1d2a538b45644890effbbc74
Jakob Tuma <jtuma@myseneca.ca>
2025-02-14 16:46:13 -0500

    Added lab5a

--- lab5a.py
+++ lab5a.py
@@ -9,7 +9,7 @@ def read_file_list(file_name):
     # Takes a file_name as string for a file name, 
     # return its entire contents as a list of lines without new-line characters
     f = open(file_name, 'r')
-    return f.read().splitlines()
+    return f.read().strip().split('\n')
 if __name__ == '__main__':
     file_name = 'data.txt'
     print(read_file_string(file_name))
